907b554b648e3e260209fa50044ab4253a053531
marl-Am <marl-Am@users.noreply.github.com>
2023-03-13 23:46:11 -0400

    Update SecurityConfiguration.java

--- src/main/java/com/marlon/blog/config/SecurityConfiguration.java
+++ src/main/java/com/marlon/blog/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import com.marlon.blog.enums.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +11,7 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true) // So we can use @PreAuthorize
+@EnableGlobalMethodSecurity(prePostEnabled = true) // So we can use @PreAuthorize
 public class SecurityConfiguration {
 
     @Bean
