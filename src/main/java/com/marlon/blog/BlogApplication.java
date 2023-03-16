9a122cb4305fbd260ed6c3e3fec98d763a1f1e08
marl-Am <marl-Am@users.noreply.github.com>
2023-03-15 21:36:38 -0400

    Changed to postgres

--- src/main/java/com/marlon/blog/BlogApplication.java
+++ src/main/java/com/marlon/blog/BlogApplication.java
@@ -1,11 +1,26 @@
 package com.marlon.blog;
 
+import com.marlon.blog.config.DatabaseWriter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class BlogApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner writeData(DataSource dataSource) {
+		return args -> {
+			DatabaseWriter databaseWriter = new DatabaseWriter(dataSource);
+			databaseWriter.writeData();
+			System.out.println("Wrote data to PostgreSQL database!");
+		};
 	}
 }
