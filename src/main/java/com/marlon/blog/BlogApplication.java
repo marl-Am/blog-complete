13894d5cfb8418c3bd2232ab1639f143d9c875d1
marl-Am <marl-Am@users.noreply.github.com>
2023-03-17 08:24:08 -0400

    Added login info

--- src/main/java/com/marlon/blog/BlogApplication.java
+++ src/main/java/com/marlon/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.marlon.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:config.properties")
 public class BlogApplication {
 	public static void main(String[] args) {
 
