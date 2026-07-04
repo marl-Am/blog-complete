c24c93cc4e8fb6176aab11e6e4a50c2cc19ec40c
marl-Am <11708748+marl-Am@users.noreply.github.com>
2026-07-04 16:34:39 -0400

    Update Constants.java

--- src/main/java/com/marlon/blog/constants/Constants.java
+++ src/main/java/com/marlon/blog/constants/Constants.java
@@ -2,12 +2,11 @@ package com.marlon.blog.constants;
 
 import java.util.Arrays;
 import java.util.List;
+
 public class Constants {
     public static final List<String> TAGS = Arrays.asList(
             "Spring", "Java", "Thymeleaf", "Security", "HTML",
-            "CSS", "JavaScript", "React", "SQL", "Python",
-            "Mobile", "Open Source", "Films", "Books", "Math",
-            "Machine Learning", "Data Science", "CI/CD", "Learning", "Other"
-    );
+            "CSS", "JavaScript", "React", "SQL", "Python", "Backend", "Frontend",
+            "Mobile", "Open Source", "Films", "Books", "Poetry", "Math", "Music",
+            "Machine Learning", "Data Science", "CI/CD", "Learning", "Other");
 }
-
