2b32b357226efe71be246da3c47a3f0197544dee
marl-Am <11708748+marl-Am@users.noreply.github.com>
2026-07-04 16:54:50 -0400

    Removed access to projects page from regular guests

--- src/main/java/com/marlon/blog/controller/ProjectsController.java
+++ src/main/java/com/marlon/blog/controller/ProjectsController.java
@@ -1,5 +1,6 @@
 package com.marlon.blog.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 public class ProjectsController {
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping
     public String viewProjects(){return "projects";}
 }
